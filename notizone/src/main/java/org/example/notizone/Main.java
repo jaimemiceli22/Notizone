d1a12d27d95a3959703c9218a501f3353012f933
JAIME\jaime <jperezmiceli@gmail.com>
2024-02-15 18:44:46 +0100

    feat:add changes

--- notizone/src/main/java/org/example/notizone/Main.java
+++ notizone/src/main/java/org/example/notizone/Main.java
@@ -9,6 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+
         try {
 
             Parent root = FXMLLoader.load(getClass().getResource("inicio.fxml"));
--- notizone/src/main/java/org/example/notizone/SceneController.java
+++ notizone/src/main/java/org/example/notizone/SceneController.java
@@ -1,19 +1,27 @@
 package org.example.notizone;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class SceneController {
 
+    @FXML
+    private ImageView imagenView;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
+
 
     public void switchToScene1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("inicio.fxml"));
@@ -37,8 +45,9 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void switchToScene4(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("agregar.fxml"));
+        root = FXMLLoader.load(getClass().getResource("agregar.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
